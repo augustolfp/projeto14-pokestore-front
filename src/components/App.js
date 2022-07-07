342ed56a760835ec61b22adac5c1dc64009bf1e4
ninaschramm <schrammnina@outlook.com>
2022-07-07 05:54:01 -0300

    Setting up userContext

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import "../assets/styles/reset.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import HomePage from "./HomePage/HomePage";
+import UserContext from "../contexts/UserContext";
 
 export default function App() {
+
+    const contextValue = {};
+
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
+        <UserContext.Provider value={ contextValue } >
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
