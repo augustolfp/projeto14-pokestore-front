cc2b741ab3e025bba74371d01a54e012a3699aeb
Augusto Lopes <augustolfp@gmail.com>
2022-07-10 17:58:50 -0300

    Sets hamburger menu as a button on TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,7 +7,7 @@ export default function TopBar(props) {
     return (
         <Container>
             <div>
-                <IoMenu/>
+                <button><IoMenu/></button>
                 <img src={logo} />
             </div>
             {props.children}
@@ -18,6 +18,12 @@ export default function TopBar(props) {
 const Container = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     background-color: #FBD743;
     color: #2774BA;
+
+    div {
+        display: flex;
+        align-items: center;
+    }
 `
